1837a9d4dc56046c5a362ffd0d9c91e0186b18df
juliaslohman <juliaslohman@gmail.com>
2020-09-16 17:27:18 -0700

    Updates

---
+++ precache-manifest.7f3a39a8caaf983225f3e7ed81a0064d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af4effffd2e79085b6a8983980bdfc6",
+    "url": "/build-a-better-boyfriend/index.html"
+  },
+  {
+    "revision": "7885254bd718cbf6aefb",
+    "url": "/build-a-better-boyfriend/static/css/main.bc100380.chunk.css"
+  },
+  {
+    "revision": "b5b3e2b210918a970c72",
+    "url": "/build-a-better-boyfriend/static/js/2.87933dd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build-a-better-boyfriend/static/js/2.87933dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7885254bd718cbf6aefb",
+    "url": "/build-a-better-boyfriend/static/js/main.e6db18ec.chunk.js"
+  },
+  {
+    "revision": "b93a315e7091e2524554",
+    "url": "/build-a-better-boyfriend/static/js/runtime-main.5f6f19ff.js"
+  },
+  {
+    "revision": "37f975610d9d8d2e7cbc8c7d0ace5c2e",
+    "url": "/build-a-better-boyfriend/static/media/50scoupleWIDE.37f97561.jpg"
+  },
+  {
+    "revision": "1c5cc3d176d39489943e897b5ee3d236",
+    "url": "/build-a-better-boyfriend/static/media/face1.1c5cc3d1.png"
+  },
+  {
+    "revision": "feaec3fca258b78a9c9f2f34034271f7",
+    "url": "/build-a-better-boyfriend/static/media/face2.feaec3fc.png"
+  },
+  {
+    "revision": "1f4f193fdb9fd7ba9a9afa221113a8fb",
+    "url": "/build-a-better-boyfriend/static/media/face3.1f4f193f.png"
+  },
+  {
+    "revision": "9f64deafb166fa1c49a9e69e6976178e",
+    "url": "/build-a-better-boyfriend/static/media/face4.9f64deaf.png"
+  },
+  {
+    "revision": "e3f59f447f5a4dfb64a13b569a28e403",
+    "url": "/build-a-better-boyfriend/static/media/face5.e3f59f44.png"
+  },
+  {
+    "revision": "756b30ecda6ba3368b8ecc9fe156534b",
+    "url": "/build-a-better-boyfriend/static/media/face6.756b30ec.png"
+  },
+  {
+    "revision": "57f0876d47b6db78e37666d564e5ed4e",
+    "url": "/build-a-better-boyfriend/static/media/face7.57f0876d.png"
+  },
+  {
+    "revision": "50dceecd061d6a579686140f3b1d30d8",
+    "url": "/build-a-better-boyfriend/static/media/face8.50dceecd.png"
+  },
+  {
+    "revision": "eb81aea2aa8add6c5c1e25db8d87133f",
+    "url": "/build-a-better-boyfriend/static/media/hermeme.eb81aea2.jpeg"
+  },
+  {
+    "revision": "8a7ae16a8586016c3fdbc235ba7efffd",
+    "url": "/build-a-better-boyfriend/static/media/retrofuturistrobotwoman.8a7ae16a.jpg"
+  },
+  {
+    "revision": "bb8ce3a9c530da857fecddb95033450c",
+    "url": "/build-a-better-boyfriend/static/media/womanandrobot2.bb8ce3a9.jpg"
+  }
+]);
